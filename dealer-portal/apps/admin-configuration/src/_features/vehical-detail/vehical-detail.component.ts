ce7ae67ae0a8969e09b6d67248ff14e757fe151a
sheetal sonsale <sheetal.sonsale@yash.com>
2025-12-03 14:28:45 +0530

    add condition report accordian

--- dealer-portal/apps/admin-configuration/src/_features/vehical-detail/vehical-detail.component.ts
+++ dealer-portal/apps/admin-configuration/src/_features/vehical-detail/vehical-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PolarisDivider, PolarisIcon, PolarisButton, PolarisAccordion } from '@dealer-portal/polaris-ui';
 import { TranslatePipe } from '@ngx-translate/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 interface VehicleSpec {
   label: string;
@@ -16,11 +17,21 @@ interface RunnerUp {
 
 @Component({
   selector: 'ac-vehical-detail',
-  imports: [CommonModule, PolarisDivider, PolarisIcon, PolarisButton, TranslatePipe],
+  imports: [
+    CommonModule,
+    PolarisDivider,
+    PolarisIcon,
+    PolarisButton,
+    TranslatePipe,
+    PolarisAccordion,
+    MatExpansionModule,
+  ],
   templateUrl: './vehical-detail.component.html',
   styleUrl: './vehical-detail.component.scss',
 })
 export class ListingDetailsComponent {
+  testId = 'listing-details';
+  showToggle = true;
   vehicleSpecs: VehicleSpec[] = [
     { label: 'Vehicle Type', value: 'Side By Side' },
     { label: 'Year', value: '2021' },
@@ -44,9 +55,342 @@ export class ListingDetailsComponent {
     },
   ];
 
+  conditionSummary = {
+    categories: [
+      {
+        categoryName: 'Exterior',
+        questions: [
+          {
+            title: 'Body',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Excellent',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#0BA300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Very Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#8CB300',
+                isSelected: true,
+              },
+              {
+                answerText: 'Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FFC600',
+                isSelected: false,
+              },
+              {
+                answerText: 'Fair',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FF7317',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Chassis/Frame',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Excellent',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#0BA300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Very Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#8CB300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FFC600',
+                isSelected: true,
+              },
+              {
+                answerText: 'Fair',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FF7317',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+        ],
+      },
+      {
+        categoryName: 'Interior',
+        questions: [
+          {
+            title: 'Seat Capacity',
+            questionText: '',
+            answers: [
+              {
+                answerText: '1',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+              {
+                answerText: '2',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Seat(s)',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Excellent',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#0BA300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Very Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#8CB300',
+                isSelected: true,
+              },
+              {
+                answerText: 'Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FFC600',
+                isSelected: false,
+              },
+              {
+                answerText: 'Fair',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FF7317',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+        ],
+      },
+      {
+        categoryName: 'Electrical',
+        questions: [
+          {
+            title: 'Infotainment/Display/Audio/Speakers',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Lights',
+            questionText: 'Present and Functioning?',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Gauges',
+            questionText: 'Functioning?',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Warning Indicators & Labels',
+            questionText: 'Present on the Vehicle?',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+        ],
+      },
+      {
+        categoryName: 'Wheels/Tires/Tracks',
+        questions: [
+          {
+            title: 'Wheels/Rims',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Excellent',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#0BA300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Very Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#8CB300',
+                isSelected: true,
+              },
+              {
+                answerText: 'Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FFC600',
+                isSelected: false,
+              },
+              {
+                answerText: 'Fair',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FF7317',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Tires',
+            questionText: '',
+            answers: [
+              {
+                answerText: 'Excellent',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#0BA300',
+                isSelected: false,
+              },
+              {
+                answerText: 'Very Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#8CB300',
+                isSelected: true,
+              },
+              {
+                answerText: 'Good',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FFC600',
+                isSelected: false,
+              },
+              {
+                answerText: 'Fair',
+                selectedColor: '#000000',
+                selectedBackgroundColor: '#FF7317',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+        ],
+      },
+      {
+        categoryName: 'Maintenance',
+        questions: [
+          {
+            title: 'Pre-Sales Maintenance',
+            questionText: 'Complete?',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+          {
+            title: 'Maintenance Schedule',
+            questionText: 'Up to date?',
+            answers: [
+              {
+                answerText: 'Yes',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: true,
+              },
+              {
+                answerText: 'No',
+                selectedColor: '#FFFFFF',
+                selectedBackgroundColor: '#004E97',
+                isSelected: false,
+              },
+            ],
+            notes: null,
+            images: [],
+          },
+        ],
+      },
+    ],
+  };
+
   // openManualBid() {
   //   var modal = document.querySelector('#staticBackdrop');
   //   if (modal)
   //   modal.modal('show')
   // }
+  public toggleAccordion(event: Event) {
+    event.preventDefault();
+  }
 }
